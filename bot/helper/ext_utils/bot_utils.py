685efc12e9b30e52bca58b94fc0d07554a01d8b9
Mehedi Hasan Shojib <107526130+5hojib@users.noreply.github.com>
2023-02-21 12:03:13 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -245,8 +245,8 @@ def get_readable_message():
                 globals()['COUNT'] -= STATUS_LIMIT
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
-            msg += f"<b> {escape(str(download.name()))}</b>"
-            msg += f"\n<b>┌<a href='{download.message.link}'>{download.status()}</a> </b>"
+            msg += f"<b>{escape(str(download.name()))}</b>"
+            msg += f"\n<b>┌<a href=' {download.message.link}'>{download.status()}</a> </b>"
             if download.status() not in [MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_CONVERTING, MirrorStatus.STATUS_QUEUEDL, MirrorStatus.STATUS_QUEUEUP]:
                 if config_dict['EMOJI_THEME']:
                     msg += f"\n<b>├ </b>{get_progress_bar_string(download)} {download.progress()}"
