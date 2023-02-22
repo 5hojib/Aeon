427316483ee68bae21bf7667078de229b9966a93
Mehedi Hasan Shojib <107526130+5hojib@users.noreply.github.com>
2023-02-22 10:43:05 +0600

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -21,29 +21,29 @@ def speedtest(update, context):
     path = (result['share'])
     currentTime = get_readable_time(time() - botStartTime)
     string_speed = f'''
-╭─《 🚀 SPEEDTEST INFO 》
-├ <b>Upload:</b> <code>{speed_convert(result['upload'], False)}</code>
-├ <b>Download:</b>  <code>{speed_convert(result['download'], False)}</code>
-├ <b>Ping:</b> <code>{result['ping']} ms</code>
-├ <b>Time:</b> <code>{result['timestamp']}</code>
-├ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
-╰ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
+<b><u>SPEEDTEST INFO</u></b>
+<b>┌ Upload:</b> <code>{speed_convert(result['upload'], False)}</code>
+<b>├ Download:</b>  <code>{speed_convert(result['download'], False)}</code>
+<b>├ Ping:</b> <code>{result['ping']} ms</code>
+<b>├ Time:</b> <code>{result['timestamp']}</code>
+<b>├ Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
+<b>└ Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
 
-╭─《 🌐 SPEEDTEST SERVER 》
-├ <b>Name:</b> <code>{result['server']['name']}</code>
-├ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-├ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-├ <b>Latency:</b> <code>{result['server']['latency']}</code>
-├ <b>Latitude:</b> <code>{result['server']['lat']}</code>
-╰ <b>Longitude:</b> <code>{result['server']['lon']}</code>
+<b><u>SPEEDTEST SERVER</u></b>
+<b>┌ Name:</b> <code>{result['server']['name']}</code>
+<b>├ Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+<b>├ Sponsor:</b> <code>{result['server']['sponsor']}</code>
+<b>├ Latency:</b> <code>{result['server']['latency']}</code>
+<b>├ Latitude:</b> <code>{result['server']['lat']}</code>
+<b>└ Longitude:</b> <code>{result['server']['lon']}</code>
 
-╭─《 👤 CLIENT DETAILS 》
-├ <b>IP Address:</b> <code>{result['client']['ip']}</code>
-├ <b>Latitude:</b> <code>{result['client']['lat']}</code>
-├ <b>Longitude:</b> <code>{result['client']['lon']}</code>
-├ <b>Country:</b> <code>{result['client']['country']}</code>
-├ <b>ISP:</b> <code>{result['client']['isp']}</code>
-╰ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+<u><b>CLIENT DETAILS</b></u>
+<b>┌ IP Address:</b> <code>{result['client']['ip']}</code>
+<b>├ Latitude:</b> <code>{result['client']['lat']}</code>
+<b>├ Longitude:</b> <code>{result['client']['lon']}</code>
+<b>├ Country:</b> <code>{result['client']['country']}</code>
+<b>├ ISP:</b> <code>{result['client']['isp']}</code>
+<b>└ ISP Rating:</b> <code>{result['client']['isprating']}</code>
 '''
     try:
         pho = sendPhoto(text=string_speed, bot=context.bot, message=update.message, photo=path)
