765fbaa90cb5d6197f26ad9da78d0401ab988eaa
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-03 16:50:59 +0600

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -242,7 +242,8 @@ def start_clone(listelem):
                 else:
                     sendMessage(msg + botpm, bot, message, buttons.build_menu(2))
             else:
-                if config_dict['PICS']:
+                    cc = f'\n<b>└ Cloned By: </b>{tag}\n\n'
+                    if config_dict['PICS']:
                     sendPhoto(result + cc, bot, message, rchoice(config_dict['PICS']), button)
                 else:
                     sendMessage(result + cc, bot, message, button)
