fa72be2d526b53b7f7c51b775140621a00d09ec3
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-10 17:44:54 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -300,7 +300,12 @@ def get_readable_message():
                     up_speed += float(spd.split('K')[0]) * 1024
                 elif 'M' in spd:
                     up_speed += float(spd.split('M')[0]) * 1048576
-        bmsg = f"<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}"
+        if config_dict['TOTAL_TASKS_LIMIT']:
+            TASKS_COUNT = f"<b>Task Limit: </b>{config_dict['TOTAL_TASKS_LIMIT']} | <b>Run:</b> {len(download_dict)} | <b>Free:</b> {config_dict['TOTAL_TASKS_LIMIT'] - len(download_dict)}\n"
+        else:
+            TASKS_COUNT = f""
+            bmsg = f"{TASKS_COUNT}"
+            bmsg += f"<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}"
         bmsg += f"\n<b>Free Disk:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
         bmsg += f"\n<b>Total Downloading Speed:</b> {get_readable_file_size(dl_speed)}/s"
         bmsg += f"\n<b>Total Uploading Speed:</b> {get_readable_file_size(up_speed)}/s"
