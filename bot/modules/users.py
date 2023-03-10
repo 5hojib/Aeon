0eef21c9be92cc3a3ca188de9fdea7e89296d25c
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-10 22:04:40 +0600

    Update users.py

--- bot/modules/users.py
+++ bot/modules/users.py
@@ -6,6 +6,46 @@ from bot import app, OWNER_ID, DATABASE_URL, dispatcher
 from bot.helper.telegram_helper.filters import CustomFilters
 
 
+def info(update, context):
+    chat = update.effective_chat
+    dc_info = context.bot_data['dc_info']
+    
+    if context.args:
+        try:
+            user_id = int(context.args[0])
+            user = context.bot.get_chat(user_id)
+        except ValueError:
+            username = context.args[0].replace("@", "")
+            user = context.bot.get_chat(username=username)
+    else:
+        if update.message.reply_to_message:
+            user = update.message.reply_to_message.from_user
+        else:
+            user = chat
+    
+    if user.type == telegram.Chat.PRIVATE:
+        profile_photo = context.bot.get_user_profile_photos(user.id).photos[-1][-1] if context.bot.get_user_profile_photos(user.id).photos else None
+        profile_link = f"https://t.me/{user.username}" if user.username else None
+        message = f"Your username is {user.username} and your ID is {user.id}. \n\n" if user == chat else ""
+    else:
+        profile_photo = None
+        profile_link = None
+        message = f"This is the {user.title} group and the group ID is {user.id}. \n\n" if user == chat else ""
+    
+    message += f"Current DC information: \n" \
+              f"DC ID: {dc_info.id}\n" \
+              f"DC IP Address: {dc_info.ip_address}\n" \
+              f"DC Port: {dc_info.port}\n\n"
+    
+    if profile_photo:
+        context.bot.send_photo(chat_id=chat.id, photo=profile_photo)
+
+    if profile_link:
+        message += f"Your permanent link is {profile_link}."
+    
+    update.message.reply_text(message)
+
+
 def dbusers(update, context):
     if not DATABASE_URL:
         context.bot.send_message(chat_id=update.effective_chat.id, text=f"DATABASE_URL not provided")
@@ -24,9 +64,10 @@ def get_id(update: Update, context):
     else:
         context.bot.send_message(chat_id=chat_id, text=f"This group's ID is: <code>{chat_id}</code>")
 
-
+info_handler = CommandHandler("info", info, filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
 dbusers_handler = CommandHandler("dbusers", dbusers, filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
 id_handler = CommandHandler("id", get_id)
 
 dispatcher.add_handler(dbusers_handler)
 dispatcher.add_handler(id_handler)
+dispatcher.add_handler(info)
