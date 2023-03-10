6e15f63964cecda52c167ee8494a0f93f2fb4950
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-10 17:51:57 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -304,8 +304,11 @@ def get_readable_message():
             TASKS_COUNT = f"<b>Task Limit: </b>{config_dict['TOTAL_TASKS_LIMIT']} | <b>Run:</b> {len(download_dict)} | <b>Free:</b> {config_dict['TOTAL_TASKS_LIMIT'] - len(download_dict)}\n"
         else:
             TASKS_COUNT = f""
+        if config_dict['EMOJI_THEME']:
             bmsg = f"{TASKS_COUNT}"
-            bmsg += f"<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}"
+        else:
+        bmsg = f"{TASKS_COUNT}"
+        bmsg += f"<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}"
         bmsg += f"\n<b>Free Disk:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
         bmsg += f"\n<b>Total Downloading Speed:</b> {get_readable_file_size(dl_speed)}/s"
         bmsg += f"\n<b>Total Uploading Speed:</b> {get_readable_file_size(up_speed)}/s"
