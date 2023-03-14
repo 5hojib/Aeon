5a8bfdf7666eb0212e95af25d620c10cf63153ba
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-14 15:22:18 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -671,7 +671,7 @@ if len(BASE_URL) == 0:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-   UPSTREAM_REPO = 'https://github.com/SN-ABDULLAH-AL-NOMAN/SN-MLTB'
+   UPSTREAM_REPO = 'https://github.com/5hojib/hk-upstream'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
