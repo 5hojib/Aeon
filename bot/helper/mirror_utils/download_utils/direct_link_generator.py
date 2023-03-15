28aeb8d1811c1034a670854b61bcfdce5534c086
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-15 20:28:03 +0600

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -14,7 +14,6 @@ from lxml import etree
 from cfscrape import create_scraper
 from bs4 import BeautifulSoup
 from base64 import standard_b64encode, b64decode
-from playwright.sync_api import Playwright, sync_playwright, expect
 
 from bot import LOGGER, config_dict
 from bot.helper.telegram_helper.bot_commands import BotCommands
@@ -790,37 +789,6 @@ def shareDrive(url, directLogin=True):
                 "ERROR! File Not Found or User rate exceeded !!")
 
 
-def prun(playwright: Playwright, link: str) -> str:
-    """ filepress google drive link generator
-    By https://t.me/maverick9099
-    GitHub: https://github.com/majnurangeela"""
-
-    browser = playwright.chromium.launch()
-    context = browser.new_context()
-
-    page = context.new_page()
-    page.goto(link)
-
-    firstbtn = page.locator(
-        "xpath=//div[text()='Direct Download']/parent::button")
-    expect(firstbtn).to_be_visible()
-    firstbtn.click()
-    sleep(6)
-
-    secondBtn = page.get_by_role("button", name="Download Now")
-    expect(secondBtn).to_be_visible()
-    with page.expect_navigation():
-        secondBtn.click()
-
-    Flink = page.url
-
-    context.close()
-    browser.close()
-
-    if 'drive.google.com' in Flink:
-        return Flink
-    else:
-        raise DirectDownloadLinkException("Unable To Get Google Drive Link!")
 
 
 def unified(link) -> str:
