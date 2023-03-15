76c3d191ceadfbf284f9c145a50888e4e6aee69c
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-15 18:08:16 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -149,7 +149,9 @@ SA_MAIL = environ.get('SA_MAIL', '')
 if len(SA_MAIL) == 0:
     SA_MAIL = '#SA'    
 
-TGH_THUMB = 'https://graph.org/file/4b95155527b50ef891d2d.jpg'
+TGH_THUMB = environ.get('TGH_THUMB', '')
+if len(TGH_THUMB) == 0:
+    TGH_THUMB = 'https://graph.org/file/4b95155527b50ef891d2d.jpg'
 
 path = "Thumbnails/"
 if not ospath.isdir(path):
