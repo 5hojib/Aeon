7ad3b7b67e2767645756775a3b19efc8034f91e8
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-16 18:05:44 +0600

    Update bot_settings.py

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -34,16 +34,16 @@ default_values = {'AUTO_DELETE_MESSAGE_DURATION': -1,
                   'DOWNLOAD_DIR': '/usr/src/app/downloads/',
                   'TIME_GAP': -1,
                   'TG_SPLIT_SIZE': tgBotMaxFileSize,
-                  'TGH_THUMB': 'https://te.legra.ph/file/3325f4053e8d68eab07b5.jpg',
-                  'START_BTN1_NAME': 'Master',
-                  'START_BTN1_URL': 'https://t.me/krn_adhikari',
+                  'TGH_THUMB': 'https://graph.org/file/4b95155527b50ef891d2d.jpg',
+                  'START_BTN1_NAME': 'Repo',
+                  'START_BTN1_URL': 'https://github.com/5hojib/hk-upstream',
                   'START_BTN2_NAME': 'Support Group',
-                  'START_BTN2_URL': 'https://t.me/WeebZone_updates',
+                  'START_BTN2_URL': 'https://t.me/hk_upstream_chat',
                   'AUTHOR_NAME': 'Chishiya',
                   'AUTHOR_URL': 'https://t.me/JuniorXcientist',
                   'TITLE_NAME': 'Chishiya',
-                  'GD_INFO': 'Uploaded by WeebZone Mirror Bot',
-                  'CREDIT_NAME': 'WeebZone',
+                  'GD_INFO': 'Uploaded by HK-Upstream',
+                  'CREDIT_NAME': 'Chishiya',
                   'NAME_FONT': 'code',
                   'CAPTION_FONT': 'code',
                   'FINISHED_PROGRESS_STR': '█',
@@ -125,7 +125,7 @@ def load_config():
 
     TGH_THUMB = environ.get('TGH_THUMB', '')
     if len(TGH_THUMB) == 0:
-        TGH_THUMB = 'https://te.legra.ph/file/3325f4053e8d68eab07b5.jpg'
+        TGH_THUMB = 'https://graph.org/file/4b95155527b50ef891d2d.jpg'
 
     SA_MAIL = environ.get('SA_MAIL', '')
     if len(SA_MAIL) == 0:
@@ -549,15 +549,15 @@ def load_config():
 
     AUTHOR_NAME = environ.get('AUTHOR_NAME', '')
     if len(AUTHOR_NAME) == 0:   
-        AUTHOR_NAME = 'WZML'
+        AUTHOR_NAME = 'Chishiya'
 
     AUTHOR_URL = environ.get('AUTHOR_URL', '')
     if len(AUTHOR_URL) == 0:    
-        AUTHOR_URL = 'https://t.me/WeebZone_updates'
+        AUTHOR_URL = 'https://t.me/JuniorXcientist'
 
     TITLE_NAME = environ.get('TITLE_NAME', '')
     if len(TITLE_NAME) == 0:    
-        TITLE_NAME = 'WeebZone'
+        TITLE_NAME = 'Chishiya'
 
     GD_INFO = environ.get('GD_INFO', '')
     if len(GD_INFO) == 0:   
@@ -565,7 +565,7 @@ def load_config():
 
     CREDIT_NAME = environ.get('CREDIT_NAME', '')
     if len(CREDIT_NAME) == 0:   
-        CREDIT_NAME = 'WeebZone'
+        CREDIT_NAME = 'Chishiya'
 
     NAME_FONT = environ.get('NAME_FONT', '')
     if len(NAME_FONT) == 0: 
