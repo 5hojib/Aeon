d71fdb3f0afe649f41b8110fa54dded32102a953
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-25 23:00:01 +0600

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -26,9 +26,10 @@ async def mirror_status(client, message):
     if count == 0:
         currentTime = get_readable_time(time() - botStartTime)
         free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-        msg = 'No Active Downloads !\n___________________________'
-        msg += f"\n<b>CPU</b>: {cpu_percent()}% | <b>FREE</b>: {free}" \
-                   f"\n<b>RAM</b>: {virtual_memory().percent}% | <b>UPTIME</b>: {currentTime}"
+        msg = 'No Active Downloads !'
+        msg += f"\n<b>• Bot Uptime</b>: {get_readable_time(time() - botStartTime)}"
+        msg += f"\n<b>• Free Disk</b>: {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+        msg += f"\n<b>• CPU</b>: {cpu_percent()}% | <b>RAM</b>: {virtual_memory().percent}%"
         reply_message = await sendMessage(message, msg)
         await auto_delete_message(message, reply_message)
     else:
