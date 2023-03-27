cbecaa7051596fffd3ab64bff4dfdff0c5f2da49
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-27 22:49:02 +0600

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -152,12 +152,7 @@ def appdrive(url: str) -> str:
         info_parsed['link_type'] = 'direct'
         data['action'] = 'direct'
     
-    response = None
-    while data['type'] <= 3:
-        try:
-            response = client.post(url, data=gen_payload(data), headers=headers).json()
-            break
-        except: data['type'] += 1
+    response = client.post(url, data=gen_payload(data), headers=headers).json()
     if 'url' in response:
         info_parsed['gdrive_link'] = response['url']
     elif 'error' in response and response['error']:
