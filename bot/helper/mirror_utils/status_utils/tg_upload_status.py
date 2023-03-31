1cdc57a8cd40cb9f3f33e05d1f05cf17193e3df0
Mehedi Hasan Shojib | Chishiya <107526130+5hojib@users.noreply.github.com>
2023-03-31 15:47:26 +0600

    Update tg_upload_status.py

--- bot/helper/mirror_utils/status_utils/tg_upload_status.py
+++ bot/helper/mirror_utils/status_utils/tg_upload_status.py
@@ -5,7 +5,7 @@ from bot.helper.ext_utils.bot_utils import (MirrorStatus,
                                             get_readable_file_size,
                                             get_readable_time)
 
-engine_ = f"pyrogram v{get_distribution('pyrogram').version}"
+engine_ = f"Pyrogram"
 
 class TgUploadStatus:
     def __init__(self, obj, size, gid, message, extra_details):
