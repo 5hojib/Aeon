294f4db03bf15f7c2944c9fcb144fb9101f361cf
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-04-15 14:54:09 +0600

    status

--- bot/helper/mirror_utils/status_utils/gdrive_status.py
+++ bot/helper/mirror_utils/status_utils/gdrive_status.py
@@ -5,7 +5,7 @@ from bot.helper.ext_utils.bot_utils import (MirrorStatus,
                                             get_readable_file_size,
                                             get_readable_time)
 
-engine_ = f"Google Api v{get_distribution('google-api-python-client').version}"
+engine_ = "Google Api"
 
 
 class GdriveStatus:
--- bot/helper/mirror_utils/status_utils/qbit_status.py
+++ bot/helper/mirror_utils/status_utils/qbit_status.py
@@ -15,7 +15,7 @@ def get_download(client, tag):
             f'{e}: Qbittorrent, while getting torrent info. Tag: {tag}')
 
 
-engine_ = f"qBittorrent {get_client().app.version}"
+engine_ = "qBittorrent"
 
 
 class QbittorrentStatus:
