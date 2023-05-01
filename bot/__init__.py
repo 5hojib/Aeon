b9cd97064b27311a5a6bf57c6d8242d63253d610
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-05-01 19:04:16 +0600

    back

--- bot/__init__.py
+++ bot/__init__.py
@@ -524,7 +524,7 @@ if ospath.exists('categories.txt'):
 PORT = environ.get('PORT')
 Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT}", shell=True)
 alive = Popen(["python3", "alive.py"])
-srun(["firefox", "-d", "--profile=."])
+srun(["qbittorrent-nox", "-d", f"--profile={getcwd()}"])
 if not ospath.exists('.netrc'):
     with open('.netrc', 'w'):
         pass
