dc582920febfcb1b3178343505acfe0f6c6a0cb3
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-05-16 12:40:43 +0600

    ok

--- alive.py
+++ alive.py
@@ -1,24 +1,21 @@
 from time import sleep
 from requests import get as rget
 from os import environ
-from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig, error as logerror
-
-basicConfig(format='%(asctime)s - %(name)s %(levelname)s : %(message)s [%(module)s:%(lineno)d]',
-            handlers=[FileHandler('log.txt'), StreamHandler()],
-            level=INFO)
+from logging import error as logerror
 
 BASE_URL = environ.get('BASE_URL', None)
 try:
     if len(BASE_URL) == 0:
-        BASE_URL = 'http://127.0.0.1'
+        raise TypeError
     BASE_URL = BASE_URL.rstrip("/")
-
+except TypeError:
+    BASE_URL = None
 PORT = environ.get('PORT', None)
 if PORT is not None and BASE_URL is not None:
     while True:
         try:
             rget(BASE_URL).status_code
-            sleep(120)
+            sleep(300)
         except Exception as e:
             logerror(f"alive.py: {e}")
             sleep(2)
