9ffa4684a90546d41ba90354701129e92a3afce1
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-06-07 23:24:32 +0600

    ff

--- bot/modules/index_scrape.py
+++ bot/modules/index_scrape.py
@@ -109,100 +109,57 @@ Example:
 
     index_link = split_text[1]
 
-    # Check if the command has any options
-    if len(split_text) > 2 and split_text[2].startswith("-"):
-        options = split_text[2:]
+    options = []
+    for option in split_text[2:]:
+        if option.startswith("-"):
+            options.append(option)
 
-        # Parse the options to extract username and password
-        username = "username-default"
-        password = "password-default"
+    username = "username-default"
+    password = "password-default"
+    send_separately = False
 
-        for i in range(0, len(options), 2):
-            if options[i] == "-u" and i + 1 < len(options):
-                username = options[i + 1]
-            elif options[i] == "-p" and i + 1 < len(options):
-                password = options[i + 1]
+    for option in options:
+        if option == "-s":
+            send_separately = True
+        elif option == "-u":
+            index = options.index(option)
+            if index + 1 < len(options):
+                username = options[index + 1]
+        elif option == "-p":
+            index = options.index(option)
+            if index + 1 < len(options):
+                password = options[index + 1]
 
-        if "-s" in options:
-            # Send each link separately
-            if message.reply_to_message and message.reply_to_message.text:
-                reply_to_text = message.reply_to_message.text
-                index_link = re.findall(r'(https?://\S+)', reply_to_text)
-                if not index_link:
-                    await client.send_message(message.chat.id, "No valid URL found in the replied message.")
-                    return
-                index_link = index_link[0]
+    if send_separately:
+        # Send each link separately
+        if message.reply_to_message and message.reply_to_message.text:
+            reply_to_text = message.reply_to_message.text
+            index_link = re.findall(r'(https?://\S+)', reply_to_text)
+            if not index_link:
+                await client.send_message(message.chat.id, "No valid URL found in the replied message.")
+                return
+            index_link = index_link[0]
 
-            result = await get_direct_download_links(index_link, username, password)
-            links = result.split('\n')
-            total_files = len(links)
-            for link in links:
-                await client.send_message(message.chat.id, link)
-                await asyncio.sleep(1)
-            completion_message = f"Your task is done, total files: {total_files}"
-            await client.send_message(message.chat.id, completion_message)
-        else:
-            # Save links to a text file
-            result = await get_direct_download_links(index_link, username, password)
-            file_path = "extracted_links.txt"
-            with open(file_path, "w") as file:
-                file.write(result)
-
-            # Send the text file as a document
-            await client.send_document(message.chat.id, file_path)
-
-            # Remove the text file
-            os.remove(file_path)
+        result = await get_direct_download_links(index_link, username, password)
+        links = result.split('\n')
+        total_files = len(links)
+        for link in links:
+            await client.send_message(message.chat.id, link)
+            await asyncio.sleep(1)
+        completion_message = f"Your task is done, total files: {total_files}"
+        await client.send_message(message.chat.id, completion_message)
     else:
-        username = "username-default"
-        password = "password-default"
+        # Save links to a text file
+        result = await get_direct_download_links(index_link, username, password)
+        file_path = "extracted_links.txt"
+        with open(file_path, "w") as file:
+            file.write(result)
 
-        if len(split_text) > 2:
-            if split_text[2] == "-s":
-                # Send each link separately
-                if message.reply_to_message and message.reply_to_message.text:
-                    reply_to_text = message.reply_to_message.text
-                    index_link = re.findall(r'(https?://\S+)', reply_to_text)
-                    if not index_link:
-                        await client.send_message(message.chat.id, "No valid URL found in the replied message.")
-                        return
-                    index_link = index_link[0]
+        # Send the text file as a document
+        await client.send_document(message.chat.id, file_path)
 
-                result = await get_direct_download_links(index_link, username, password)
-                links = result.split('\n')
-                total_files = len(links)
-                for link in links:
-                    await client.send_message(message.chat.id, link)
-                    await asyncio.sleep(1)
-                completion_message = f"Your task is done, total files: {total_files}"
-                await client.send_message(message.chat.id, completion_message)
-            else:
-                help_message = """Invalid option provided. Please use the /index command followed by the index link.
-
-Usage:
-/index index_link
-
-Options:
-• -s: Send each link separately.
-• -u: Username
-• -p: Password
-
-Example:
-/index https://example.com/index.html -s -u your_username -p your_password
-"""
-                await client.send_message(message.chat.id, help_message)
-        else:
-            # Save links to a text file
-            result = await get_direct_download_links(index_link, username, password)
-            file_path = "extracted_links.txt"
-            with open(file_path, "w") as file:
-                file.write(result)
-
-            # Send the text file as a document
-            await client.send_document(message.chat.id, file_path)
-
-            # Remove the text file
-            os.remove(file_path)
+        # Remove the text file
+        os.remove(file_path)
 
 
 bot.add_handler(MessageHandler(extract_url, filters=command("index")))
