7588f9cb815b61c00de0cb300e918a01908ae049
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-06-07 18:53:10 +0600

    ok

--- bot/modules/index_scrape.py
+++ bot/modules/index_scrape.py
@@ -1,4 +1,6 @@
+import asyncio
 import os
+
 from json import loads as jloads
 from requests import post as rpost
 from base64 import b64encode, b64decode
@@ -56,18 +58,24 @@ async def get_direct_download_links(url, username="none", password="none"):
 
     return '\n'.join(links)
     
-    
 async def extract_url(client, message):
     if len(message.text.split()) < 2:
         await client.send_message(message.chat.id, "No index link provided. Please use the /index command followed by the index link.")
         return
 
-    if message.text.endswith('/'):
-        index_link = message.text.split()[1]
-        username = "username-default"
-        password = "password-default"
-        result = await get_direct_download_links(index_link, username, password)
+    split_text = message.text.split()
+    index_link = split_text[1]
+    username = "username-default"
+    password = "password-default"
+    result = await get_direct_download_links(index_link, username, password)
 
+    if len(split_text) > 2 and split_text[2] == "separate":
+        # Send each link separately with a delay of one second
+        links = result.split('\n')
+        for link in links:
+            await client.send_message(message.chat.id, link)
+            await asyncio.sleep(1)
+    else:
         # Save links to a text file
         file_path = "extracted_links.txt"
         with open(file_path, "w") as file:
