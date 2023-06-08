53972b46f4f936345a2d47529ca7ef9bd70cf9c3
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-06-08 19:08:52 +0600

    ok

--- bot/modules/index_scrape.py
+++ bot/modules/index_scrape.py
@@ -63,13 +63,15 @@ async def get_direct_download_links(url, username="none", password="none"):
 
 
 async def extract_url(client, message):
-    if len(message.text.split()) < 2:
-        # Check if message is a reply and extract the URL from the replied message
-        if message.reply_to_message and message.reply_to_message.text:
-            reply_to_text = message.reply_to_message.text
-            match = re.match(r"/index(?:\s(-s))?(?:\s(-u)\s(\S+))?(?:\s(-p)\s(\S+))?", reply_to_text.strip())
+    # Check if the message is a reply and extract the URL from the replied message
+    if message.reply_to_message and message.reply_to_message.text:
+        reply_to_text = message.reply_to_message.text
+        url_match = re.search(r"(?P<url>https?://[^\s]+)", reply_to_text)
+        if url_match:
+            url = url_match.group("url")
+            match = re.match(r"/index(?:\s(-s))?(?:\s(-u)\s(\S+))?(?:\s(-p)\s(\S+))?", message.text.strip())
             if match:
-                index_link = match.group(0)
+                index_link = url
                 send_separately = bool(match.group(1))
                 username = match.group(3)
                 password = match.group(5)
@@ -92,39 +94,7 @@ Example:
             else:
                 return
         else:
-            help_message = """No index link provided. Please use the /index command followed by the index link.
-
-Usage:
-/index index_link
-
-Options:
-• -s: Send each link separately.
-• -u: Username
-• -p: Password
-
-Example:
-/index https://example.com/index.html -s -u your_username -p your_password
-"""
-            await client.send_message(message.chat.id, help_message)
             return
-    else:
-        split_text = message.text.split()
-
-        if split_text[0] != "/index":
-            return
-
-        index_link = split_text[1]
-        send_separately = False
-        username = "username-default"
-        password = "password-default"
-
-        for i in range(2, len(split_text)):
-            if split_text[i] == "-s":
-                send_separately = True
-            elif split_text[i] == "-u" and i + 1 < len(split_text):
-                username = split_text[i + 1]
-            elif split_text[i] == "-p" and i + 1 < len(split_text):
-                password = split_text[i + 1]
 
     if send_separately:
         # Send each link separately
