082eaa0f056f1cbc94ff82c0f1e465dd7be2e727
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-08-29 22:27:28 +0600

    vv

--- bot/__init__.py
+++ bot/__init__.py
@@ -557,7 +557,8 @@ else:
     qb_client.app_set_preferences(qb_opt)
 
 log_info("Creating client from BOT_TOKEN")
-bot = tgClient('bot', TELEGRAM_API, TELEGRAM_HASH, bot_token=BOT_TOKEN, workers=1000, parse_mode=enums.ParseMode.HTML).start()
+bot = tgClient('bot', TELEGRAM_API, TELEGRAM_HASH, bot_token=BOT_TOKEN, workers=1000,
+               parse_mode=enums.ParseMode.HTML).start()
 bot_loop = bot.loop
 bot_name = bot.me.username
 scheduler = AsyncIOScheduler(timezone=str(
