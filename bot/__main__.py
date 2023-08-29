cf87ac64f3d80c161405d18c1b76e4c3f712bbb6
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-08-29 06:22:34 +0600

    ff

--- bot/__main__.py
+++ bot/__main__.py
@@ -32,7 +32,7 @@ from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
 from .helper.listeners.aria2_listener import start_aria2_listener
 from .modules import authorize, cancel_mirror, mirror_leech, status, torrent_search, torrent_select, ytdlp, rss, shell, eval, users_settings, bot_settings, speedtest, images, mediainfo, broadcast
-from .helper.mirror_utils.gdrive_utils count, delete, list, clone
+from .helper.mirror_utils.gdrive_utils import count, delete, list, clone
 
 @new_thread
 async def stats(_, message):
