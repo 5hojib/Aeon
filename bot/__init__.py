8eb49b7d6f488937c1833fa16a3a4b3ffec77c9a
Mehedi Hasan Shojib <yesiamshojib@gmail.com>
2023-09-07 16:40:39 +0600

    AUTH_KEY_DUPLICATE issue fix.

--- bot/__init__.py
+++ bot/__init__.py
@@ -165,7 +165,7 @@ USER_SESSION_STRING = environ.get('USER_SESSION_STRING', '')
 if len(USER_SESSION_STRING) != 0:
     log_info("Creating client from USER_SESSION_STRING")
     try:
-        user = tgClient('user', TELEGRAM_API, TELEGRAM_HASH, session_string = USER_SESSION_STRING, workers = 1000, parse_mode = enums.ParseMode.HTML).start()
+        user = tgClient('user', TELEGRAM_API, TELEGRAM_HASH, session_string = USER_SESSION_STRING, workers = 1000, parse_mode = enums.ParseMode.HTML, no_updates = True).start()
         IS_PREMIUM_USER = user.me.is_premium
     except Exception as e:
         log_error(f"Failed making client from USER_SESSION_STRING : {e}")
