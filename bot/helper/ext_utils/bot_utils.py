4d09e3846a28fd668a679b2f00da060423a034da
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-09-08 11:46:57 +0600

    okhe

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -277,7 +277,7 @@ def is_telegram_link(url):
 
 
 def is_share_link(url):
-    return bool(re_match(r'https?:\/\/.+\.gdtot\.\S+|https?:\/\/(filepress|hubdrive|filebee|appdrive|gdflix)\.\S+', url))
+    return bool(re_match(r'https?://(new\.filepress\.store|.+\.gdtot\.\S+|filepress|hubdrive|filebee|appdrive|gdflix)\b', url))
 
 
 def is_mega_link(url):
