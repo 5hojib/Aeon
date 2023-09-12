40ac496f4bab93c82bf9d6b1f39dcf15e93cad42
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-09-12 19:56:29 +0600

    ff

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -185,8 +185,8 @@ async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None)
     return text, button
 
 
-async def update_user_settings(query, key=None, edit_type=None, edit_mode=None, msg=None):
-    msg, button = await get_user_settings(msg.from_user, key, edit_type, edit_mode)
+async def update_user_settings(query, key=None, edit_type=None, edit_mode=None, msg=None, sdirect=False):
+    msg, button = await get_user_settings(msg.from_user  if sdirect else query.from_user, key, edit_type, edit_mode)
     user_id = query.from_user.id
     thumbnail = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(thumbnail):
