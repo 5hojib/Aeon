49617f9f716e7986e28b9dda8ea8b625ffde8ca5
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-09-12 21:20:59 +0600

    to f

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -187,10 +187,7 @@ async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None)
 
 async def update_user_settings(query, key=None, edit_type=None, edit_mode=None, msg=None, sdirect=False):
     msg, button = await get_user_settings(msg.from_user if sdirect else query.from_user, key, edit_type, edit_mode)
-    thumbnail = f"Thumbnails/{user_id}.jpg"
-    if not ospath.exists(thumbnail):
-        thumbnail = "https://graph.org/file/25545597de34c640b31d6.jpg"
-    await editMessage(query if sdirect else query.message, msg, button, photo=thumbnail)
+    await editMessage(query if sdirect else query.message, msg, button)
 
 
 @new_thread
