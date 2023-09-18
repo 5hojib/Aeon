4232774e7cbd3bd61d2a6fe55b32b434fec1a43f
PAITxBOT <143090682+PAITxBOT@users.noreply.github.com>
2023-09-18 11:40:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,11 +2,11 @@
 
 class script(object):
   
-    USER_SETTINGS_TEXT = """<b><u>🎭 ᴜsᴇʀ sᴇᴛᴛɪɴɢs 📌</u>
+    USER_SETTINGS_TEXT = """<b><u>✨ ᴜsᴇʀ sᴇᴛᴛɪɴɢs</u></b>
     
 ‣ ɴᴀᴍᴇ : {}
-‣ ᴜsᴇʀ ɴᴀᴍᴇ : <code>{}</code>
-‣ ᴜsᴇʀ ɪᴅ : {}
+‣ ᴜsᴇʀ ɴᴀᴍᴇ : <code>{}</code> ({})
 ‣ ʟᴀɴɢᴜᴀɢᴇ : {}
+‣ ᴛᴇʟᴇɢʀᴀᴍ ᴅᴄ : {}
 
-🫅 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ » <a href='https://t.me/Hari_OP'>ʜᴀʀɪ ᠰ ᴛɢ​</a></b>"""
+<b>🫅 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ » <a href='https://t.me/Hari_OP'>ʜᴀʀɪ ᠰ ᴛɢ​</a></b>"""
