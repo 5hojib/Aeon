a4f5be2eb9eefbfe07bc1af5e3298d0198134a23
PAITxBOT <143090682+PAITxBOT@users.noreply.github.com>
2023-09-18 15:13:53 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -157,6 +157,7 @@ def source(self):
 
 def get_readable_message():
     #msg = '<b>Powered by Aeon</b>\n\n'
+    msg = f"<b>📁 ғɪʟᴇ ɴᴀᴍᴇ</b> : <code>{escape(f'{download.name()}')}</code>\n\n"
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
@@ -169,8 +170,8 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        msg = f"<b>📁 ғɪʟᴇ ɴᴀᴍᴇ</b> : <code>{escape(f'{download.name()}')}</code>\n\n"
-        #msg += f"by {source(download)}\n\n"
+        #msg = f"<b>📁 ғɪʟᴇ ɴᴀᴍᴇ</b> : <code>{escape(f'{download.name()}')}</code>\n\n"
+        msg += f"by {source(download)}\n\n"
         #msg += f"<b>{download.status()}...</b>" 
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n⏳ <b><a href='https://t.me/Hari_OP'>{progress_bar(download.progress())} {download.progress()}</a></b>"
