4fe939020a70dd598a2b7a3efd8f1c793d4afa30
PAITxBOT <143090682+PAITxBOT@users.noreply.github.com>
2023-09-18 11:35:53 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,12 @@
+#hari codes
+
+class script(object):
+  
+    USER_SETTINGS_TEXT = """<b><u>🎭 ᴜsᴇʀ sᴇᴛᴛɪɴɢs 📌</u>
+    
+‣ ɴᴀᴍᴇ : {}
+‣ ᴜsᴇʀ ɴᴀᴍᴇ : <code>{}</code>
+‣ ᴜsᴇʀ ɪᴅ : {}
+‣ ʟᴀɴɢᴜᴀɢᴇ : {}
+
+🫅 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ » <a href='https://t.me/Hari_OP'>ʜᴀʀɪ ᠰ ᴛɢ​</a></b>"""
