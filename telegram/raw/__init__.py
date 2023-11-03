7ae537e3519d6c412cd76b3af84f9704e3e04e21
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-11-03 11:30:19 +0600

    update __init__.py

--- telegram/raw/__init__.py
+++ telegram/raw/__init__.py
@@ -3,6 +3,9 @@ from importlib import import_module
 from pyrogram.raw import types, functions, base, core
 from pyrogram.raw.all import objects
 
-for k, v in objects.items():
+try:
+	for k, v in objects.items():
     path, name = v.rsplit(".", 1)
     objects[k] = getattr(import_module(path), name)
+except:
+	pass
