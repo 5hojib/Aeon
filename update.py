745c384852ad835e47e78a347713cab17ee045cf
harixtg <150332613+harixtg@users.noreply.github.com>
2023-11-09 21:12:52 +0530

    Update update.py

--- update.py
+++ update.py
@@ -50,7 +50,7 @@ if DATABASE_URL:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://github.com/5hojib/Aeon'
+    UPSTREAM_REPO = 'https://github.com/harixtg/Aeon'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
