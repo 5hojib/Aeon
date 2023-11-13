258cdebd4c9c0c4a55a257ec0698adf56e5d803b
nero-bc <126649764+nero-bc@users.noreply.github.com>
2023-11-13 14:45:07 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -157,7 +157,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b>Powered by Aeon</b>\n\n'
+    msg = '<b>Bot Developer- @StupidBoi69</b>\n\n'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
