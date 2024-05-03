fcc6bc76ca0968ac71225a43dfff963c9492c70b
Reaper1965 <112369175+Reaper1965@users.noreply.github.com>
2024-05-03 16:13:35 +0530

    Fixed Status Error

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -20,7 +20,7 @@ async def mirror_status(_, message):
         free = get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)
         quote = Quote.print().split('―', 1)[0].strip().replace("“", "").replace("”", "")
 
-        msg = f'<b>{quote}</b>\n\n'
+        msg = f''
         msg += 'No downloads are currently in progress.\n'
         msg += f"\n<b>• Bot uptime</b>: {currentTime}"
         msg += f"\n<b>• Free disk space</b>: {free}"
