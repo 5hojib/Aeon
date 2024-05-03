1419456c1be12fc07e88bd003888a14cf3f33bec
5hojib <yesiamshojib@gmail.com>
2024-05-03 17:11:13 +0600

    update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -22,6 +22,9 @@ from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 from bot.helper.ext_utils.help_messages import PASSWORD_ERROR_MESSAGE
 
 _caches = {}
+user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0"
+)
 domain_dict = {
     'mediafire':    ['mediafire.com'],
     'osdn':         ['osdn.net'],
