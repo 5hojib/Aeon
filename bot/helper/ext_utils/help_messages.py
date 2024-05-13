135f786f02178c2850649842bca48f1bb6128ab7
5hojib <yesiamshojib@gmail.com>
2024-05-13 21:50:41 +0600

    update help_messages.py

--- bot/helper/ext_utils/help_messages.py
+++ bot/helper/ext_utils/help_messages.py
@@ -4,23 +4,15 @@ YT_HELP_MESSAGE = """
 <code>/{cmd} options</code>
 
 <b>OPTIONS:</b>
-<b>-s:</b> Select quality for specific link or links.
-
-<b>-z password:</b> Create a password-protected zip file.
-
-<b>-n new_name:</b> Rename the file.
-
-<b>-t thumbnail url:</b> Custom thumbnail for each leexh.(raw or tg image url)
-
-<b>-ss value:</b> Generate ss for leech video, max 10 for each leach.
-
-<b>-id drive_folder_link or drive_id -index https://anything.in/0:</b> Upload to a custom drive.
-
-<b>-opt playliststart:^10|fragment_retries:^inf|matchtitle:S13|writesubtitles:true|live_from_start:true|postprocessor_args:{{"ffmpeg": ["-threads", "4"]}}|wait_for_video:(5, 100):</b> Set additional options.
-
-<b>-i 10:</b> Process multiple links.
-
-<b>-b:</b> Perform bulk download by replying to a text message or file with links separated with new line.
+<blockquote><b>-s:</b> Select quality for specific link or links.</blockquote>
+<blockquote><b>-z password:</b> Create a password-protected zip file.</blockquote>
+<blockquote><b>-n new_name:</b> Rename the file.</blockquote>
+<blockquote><b>-t thumbnail url:</b> Custom thumbnail for each leech(raw or tg image url).</blockquote>
+<blockquote><b>-ss value:</b> Generate ss for leech video, max 10 for each leach.</blockquote>
+<blockquote><b>-id drive_folder_link or drive_id -index https://anything.in/0:</b> Upload to a custom drive.</blockquote>
+<blockquote><b>-opt playliststart:^10|fragment_retries:^inf|matchtitle:S13|writesubtitles:true|live_from_start:true|postprocessor_args:{{"ffmpeg": ["-threads", "4"]}}|wait_for_video:(5, 100):</b> Set additional options.</blockquote>
+<blockquote><b>-i 10:</b> Process multiple links.</blockquote>
+<blockquote><b>-b:</b> Perform bulk download by replying to a text message or file with links separated with new line.</blockquote>
 
 
 <b>Check all yt-dlp api options from this <a href='https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/YoutubeDL.py#L184'>FILE</a> or use this <a href='https://t.me/mltb_official_channel/177'>script</a> to convert cli arguments to api options.</b>
