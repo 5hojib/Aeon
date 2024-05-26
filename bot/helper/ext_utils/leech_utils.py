ca71b7e44140d65ddb8598aee4b3c88fd4148342
5hojib <yesiamshojib@gmail.com>
2024-05-27 01:07:33 +0600

    update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -332,7 +332,6 @@ def get_md5_hash(up_path):
 
 
 async def change_metadata(file, key):
-    if not file.lower().endswith('.mkv'):
-        return
-    cmd = ['render', '-i', file, '-c', 'copy', '-metadata:s:v', f'title={key}', '-metadata:s:a', f'title={key}', '-metadata:s:s', f'title={key}', file]
-    await create_subprocess_exec(*cmd, stderr=PIPE)
+    if file.lower().endswith('.mkv'):
+        cmd = ['render', '-i', file, '-c', 'copy', '-metadata:s:v', f'title={key}', '-metadata:s:a', f'title={key}', '-metadata:s:s', f'title={key}', file]
+        await create_subprocess_exec(*cmd, stderr=PIPE)
