82bd4f7c1ac70ddcc0fcd9a6142ca9f5025001ad
5hojib <yesiamshojib@gmail.com>
2024-05-30 01:18:19 +0600

    update gdriveTools.py

--- bot/helper/mirror_leech_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_leech_utils/upload_utils/gdriveTools.py
@@ -263,7 +263,8 @@ class GoogleDriveHelper:
 
     @retry(wait=wait_exponential(multiplier=2, min=3, max=6), stop=stop_after_attempt(3), retry=(retry_if_exception_type(Exception)))
     def __upload_file(self, file_path, file_name, mime_type, dest_id, is_dir=True):
-        file_name, _ = async_to_sync(format_filename, file_name, self.__user_id, file_path, True)
+        location = ospath.dirname(file_path)
+        file_name, _ = async_to_sync(format_filename, file_name, self.__user_id, location, True)
         file_metadata = {
             'name': file_name,
             'description': 'Uploaded by Aeon',
