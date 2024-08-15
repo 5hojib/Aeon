50c8e4fd8e9cfad227e57ddc85c1cc140be56197
RulfEnco <telegramencbot@outlook.com>
2024-08-15 15:28:03 +0530

    Update nsfw_check.py

--- bot/helper/aeon_utils/nsfw_check.py
+++ bot/helper/aeon_utils/nsfw_check.py
@@ -2,32 +2,7 @@ from re import IGNORECASE, search, escape
 
 
 nsfw_keywords = [
-    "porn",
-    "onlyfans",
-    "nsfw",
-    "Brazzers",
-    "adult",
-    "xnxx",
-    "xvideos",
-    "nsfwcherry",
-    "hardcore",
-    "Pornhub",
-    "xvideos2",
-    "youporn",
-    "pornrip",
-    "playboy",
-    "hentai",
-    "erotica",
-    "blowjob",
-    "redtube",
-    "stripchat",
-    "camgirl",
-    "nude",
-    "fetish",
-    "cuckold",
-    "orgy",
-    "horny",
-    "swingers",
+    "වැල්_බලන්න_එපා!"
 ]
 
 
