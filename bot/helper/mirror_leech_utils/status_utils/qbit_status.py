0d011f9b328e5f17340cb249371a8ae6c57c1d5f
Sehrish Noor <122004828+Angel-noori@users.noreply.github.com>
2024-09-14 03:50:20 -0700

    fix: 'NoneType' object has no attribute 'state' (#138)
    Bug Fixes:
    
    - Fix the 'NoneType' object has no attribute 'state' error by ensuring the client is passed correctly to the get_download function.
    
    Enhancements:
    
    - Introduce a client method in the QbittorrentStatus class to access the client instance.

--- bot/helper/mirror_leech_utils/status_utils/qbit_status.py
+++ bot/helper/mirror_leech_utils/status_utils/qbit_status.py
@@ -9,9 +9,9 @@ from bot.helper.ext_utils.bot_utils import (
 )
 
 
-def get_download(tag):
+def get_download(client, tag):
     try:
-        return xnox_client.torrents_info(tag=tag)[0]
+        return client.torrents_info(tag=tag)[0]
     except Exception as e:
         LOGGER.error(f"{e}: Qbittorrent, while getting torrent info. Tag: {tag}")
         return None
@@ -19,14 +19,15 @@ def get_download(tag):
 
 class QbittorrentStatus:
     def __init__(self, listener, seeding=False, queued=False):
+        self.__client = xnox_client
         self.__listener = listener
-        self.__info = None
+        self.__info = get_download(self.__client, f'{self.__listener.uid}')
         self.queued = queued
         self.seeding = seeding
         self.message = listener.message
 
     def __update(self):
-        new_info = get_download(f"{self.__listener.uid}")
+        new_info = get_download(self.__client, f"{self.__listener.uid}")
         if new_info is not None:
             self.__info = new_info
 
@@ -92,6 +93,9 @@ class QbittorrentStatus:
     def hash(self):
         self.__update()
         return self.__info.hash
+    
+    def client(self):
+        return self.__client
 
     def listener(self):
         return self.__listener
@@ -99,7 +103,7 @@ class QbittorrentStatus:
     async def cancel_download(self):
         self.__update()
         await sync_to_async(
-            xnox_client.torrents_pause, torrent_hashes=self.__info.hash
+            self.__client.torrents_pause, torrent_hashes=self.__info.hash
         )
         if not self.seeding:
             if self.queued:
@@ -110,10 +114,11 @@ class QbittorrentStatus:
                 msg = "Download stopped by user!"
             await sleep(0.3)
             await sync_to_async(
-                xnox_client.torrents_delete,
+                self.__client.torrents_delete,
                 torrent_hashes=self.__info.hash,
                 delete_files=True,
             )
+            await sync_to_async(self.__client.torrents_delete_tags, tags=self.__info.tags)
             async with qb_listener_lock:
                 if self.__info.tags in QbTorrents:
                     del QbTorrents[self.__info.tags]
