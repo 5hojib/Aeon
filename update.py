74a877ede60a5d4d8065f2a3742ff46ce4a672df
Mehedi Hasan Shojib <yesiamshojib@gmail.com>
2024-09-17 18:02:50 +0600

    Update update.py

--- update.py
+++ update.py
@@ -83,7 +83,7 @@ if DATABASE_URL is not None:
 
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = None
+    UPSTREAM_REPO = "https://github.com/5hojib/Aeon"
 
 UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
 if len(UPSTREAM_BRANCH) == 0:
